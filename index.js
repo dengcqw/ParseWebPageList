a8976c7825f43e8ddd80f333b9c276378566f904
dengjinlong <dengjinlong@qiyi.com>
2017-04-18 19:36:45 +0800

    use Promise.all to collect all result

--- index.js
+++ index.js
@@ -6,22 +6,43 @@ var siteMgr = require('./site.manager.js');
 var loadWebPage = require('./loadWebPage.js'); // Promise
 
 
-_.forEach(urlConfig, (urls, siteID)=>{
-  var queryFn = siteMgr.queryMap[siteID];
-  if (queryFn == undefined) {
-    console.log(key + ' not define query function');
-    return;
-  }
-  _.forEach(urls, (url, categoryID)=>{
-    console.log("----> start fetch:" + siteID + '  ' + categoryID +'  ' + url);
-    loadWebPage(url)
-    .then(html=>siteMgr.parseTitleAndURL(queryFn, html))
-    .then(hotList=>{
-      console.log('Get hot list of '+siteID+' '+categoryID+ ' length: ' + hotList.items.length);
-      let cloneItem = Object.assign({}, hotList);
-      cloneItem.siteID = siteID;
-      cloneItem.categoryID = categoryID;
-      //console.log(cloneItem);
+function start() {
+  var startTime = Date.now();
+
+  _.forEach(urlConfig, (urls, siteID)=>{
+    var queryFn = siteMgr.queryMap[siteID];
+    if (queryFn == undefined) {
+      console.log(key + ' not define query function');
+      return;
+    }
+
+    // wrap url-fetch and html-parse for Promise.all
+    var transformWrappers = _.map(urls, (url, categoryID)=>{
+
+      console.log("----> start fetch:" + siteID + '  ' + categoryID +'  ' + url);
+
+      return loadWebPage(url)
+        .then(html=>siteMgr.parseTitleAndURL(queryFn, html))
+        .then(hotList=>{
+
+          console.log('Get hot list of '+siteID+' '+categoryID+ ' length: ' + hotList.items.length);
+
+          let cloneItem = Object.assign({}, hotList);
+          cloneItem.siteID = siteID;
+          cloneItem.categoryID = categoryID;
+          cloneItem.time = Date.now() - startTime;
+          return hotList;
+        });
+    });
+
+    Promise.all(transformWrappers).then((results)=>{
+      console.log(JSON.stringify(results));
     });
   });
-});
+}
+
+function ParseWebPageList(outputPath) {
+  this.outputPath = outputPath;
+}
+
+module.exports = ParseWebPageList;
